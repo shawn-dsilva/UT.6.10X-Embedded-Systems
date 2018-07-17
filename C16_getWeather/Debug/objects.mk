eaedc96b19592b66ef464762eab841d98b75d801
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2018-07-17 17:13:22 +0530

    removed unnecessary starter files

--- C16_getWeather/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"D:/Dropbox/ArmBook/LaunchPad/ValvanoWareTM4C123/driverlib/ccs/Debug/driverlib.lib"
-
